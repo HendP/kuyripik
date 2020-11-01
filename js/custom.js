b5d3d31be924ef669f1b88731a2b449cc2beb04c
HendP <hendpermana12@gmail.com>
2020-11-01 21:27:30 +0700

    init

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+});
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+})
